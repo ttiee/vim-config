a4d6ea1642fa055b985b201bba5af7813ddb7c27
ttiee <469784630@qq.com>
2023-12-26 23:51:13 +0800

    ✨ Feat | Refact: (Plugins) Add and Style Add [nvim-treesitter/nvim-treesitter](https://github.com/nvim-treesitter/nvim-treesitter) -- nvim-tree map.set('n', 'tr', ':NvimTreeToggle<CR>') Style: Plugin Manager

--- lua/plugins/plugin-manager.lua
+++ lua/plugins/plugin-manager.lua
@@ -50,7 +50,7 @@ local treesitter = {
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "lua", "python" },
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "python" },
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = true },  
