e88b4d21d140bbe5716d0336544a8d19cef52c02
ttiee <469784630@qq.com>
2024-01-06 11:48:42 +0800

    🍭 Feat: Add copilot.nvim

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -9,6 +9,15 @@ opt.expandtab = true
 opt.autoindent = true
 
 --
-opt.wrap = false
+opt.wrap = true
 
 opt.scrolloff = 8
+
+opt.tabstop = 4
+-- opt.enpandtab = true
+opt.shiftwidth = 4
+opt.softtabstop = 4
+opt.smartindent = true
+
+--
+-- opt.encoding = "utf-8"
--- lua/plugins/plugin-manager.lua
+++ lua/plugins/plugin-manager.lua
@@ -58,9 +58,66 @@ local treesitter = {
     end
  }
 -----------------------copilot------------------------------
-local copilot = {
-        "github/copilot.vim",
+local copilot_vim = {
+    "github/copilot.vim",
 }
+
+vim.cmd [[
+let g:copilot_filetypes = { 'markdown': v:true, }
+]]
+
+local copilot_lua = {
+    "zbirenbaum/copilot.lua",
+    config = function ()
+    require("copilot").setup({
+        panel = {
+            enabled = true,
+            auto_refresh = false,
+            keymap = {
+              jump_prev = "[[",
+              jump_next = "]]",
+              accept = "<CR>",
+              refresh = "gr",
+              open = "<M-CR>"
+            },
+            layout = {
+              position = "bottom", -- | top | left | right
+              ratio = 0.4
+            },
+          },
+        suggestion = {
+            enabled = true,
+            auto_trigger = false,
+            debounce = 75,
+            keymap = {
+              accept = "<M-l>",
+              accept_word = false,
+              accept_line = false,
+              next = "<M-]>",
+              prev = "<M-[>",
+              dismiss = "<C-]>",
+            },
+          },
+        filetypes = {
+            yaml = false,
+            markdown = true,
+            help = false,
+            gitcommit = false,
+            gitrebase = false,
+            hgcommit = false,
+            svn = false,
+            cvs = false,
+            ["."] = false,
+          },
+        copilot_node_command = 'node', -- Node.js version must be > 18.x
+        server_opts_overrides = {},
+    })
+    end
+}
+
+-- vim.g.copilot_filetypes = {
+--        'markdown': true,
+-- }
 
 
 ------------------------Plugin Manager------------------------
@@ -71,7 +128,7 @@ local plugins = {
     nvim_autopairs,    -- 自动补全括号
     treesitter,    -- 语法高亮
     {'neoclide/coc.nvim', branch = 'release'},
-        copilot
+    copilot_vim,
 }
 local opts = {} -- 注意要定义这个变量
 
